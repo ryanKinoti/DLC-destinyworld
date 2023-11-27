6658b51743e6dae323564843e795b3bbffa39f3a
Ryan Kinoti <ryankinotikathurima@gmail.com>
2023-11-28 01:30:27 +0300

    switching the project to being framework based

--- routes/Auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { newUser, login } = require("../controllers/auth");
-const route = express.Router();
-
-route.post("/register", newUser);
-route.post("/login", login);
-
-module.exports = route;
--- routes/History.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const route = express.Router();
-const { getHistory } = require("../controllers/History");
-
-route.get("/:id", getHistory);
-
-module.exports = route;
